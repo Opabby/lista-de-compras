4f6d174db7178380154ade8c07b33cdde534d5f2
Thaís Rodeiro <opabby82@gmail.com>
2024-09-06 22:54:33 -0300

    Adicionando itens da lista dinamicamente enquanto mantenho o estilo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
@@ -9,19 +10,40 @@ function adicionarItem(e) {
 
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
-    containerItemLista.classList.add("lista-item-container");
+    containerItemLista.classList.add("item-lista-container");
 
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("container-checkbox");
+    
+    const checkBoxInput = document.createElement("input");
+    checkBoxInput.classList.add("checkbox-input");
+    checkBoxInput.type = "checkbox";
+    checkBoxInput.id = "checkbox-" + contador++;
+
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkBoxInput.id);
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkBoxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerCheckbox.appendChild(checkboxLabel);
+    
     const containerNomeDoItem = document.createElement("div");
+    containerNomeDoItem.classList.add("container-nome-compra");
     const nomeDoItem = document.createElement("p");
     nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(containerCheckbox);
     containerNomeDoItem.appendChild(nomeDoItem);
 
     const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
-    botaoRemover.classList.add("item-lista-button", "botao-acao");
+    botaoRemover.classList.add("botao-acao");
 
     const botaoEditar = document.createElement("button");
-    botaoEditar.classList.add("item-lista-button", "botao-acao");
+    botaoEditar.classList.add("botao-acao");
 
     const imagemRemover = document.createElement("img");
     imagemRemover.src = "img/delete.svg";
