d180c73fc342eb271fb12b475973d1e2d73e4b3c
Thaís Rodeiro <opabby82@gmail.com>
2024-09-06 21:50:44 -0300

    Criando elementos da lista dinamicamente

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const item = document.getElementById("input-item");
+const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(e) {
+    e.preventDefault();
+
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("item-lista-button", "botao-acao");
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("item-lista-button", "botao-acao");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoRemover.appendChild(imagemRemover);
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoRemover);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+}
