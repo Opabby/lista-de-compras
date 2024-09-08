5029a89f5e0d6f78df4110f9cff84116842380f8
Thaís Rodeiro <opabby82@gmail.com>
2024-09-08 01:02:27 -0300

    Adicionando data e hora

--- scripts.js
+++ scripts.js
@@ -77,6 +77,13 @@ function adicionarItem(e) {
 
     containerItemLista.appendChild(containerNomeDoItem);
     containerItemLista.appendChild(containerBotoes);
+
+    const itemData = document.createElement("p");
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long" })} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", { hour: "numeric", minute: "numeric" })}`;
+
+    itemData.classList.add("item-lista-texto");
+
     itemDaLista.appendChild(containerItemLista);
+    itemDaLista.appendChild(itemData);
     listaDeCompras.appendChild(itemDaLista);
 }
